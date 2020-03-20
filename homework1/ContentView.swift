5328d27243959a5f9499cceba4e7447bd1b85dd1
andycheng <andychz0519@gmail.com>
2020-03-20 22:36:50 +0800

    Initial Commit

---
+++ homework1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  homework1
+//
+//  Created by User12 on 2020/3/20.
+//  Copyright © 2020 User12. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
